656fc0268cd76e7f30699475d095c73d9b9d2f55
rigo <rigobertoramirez606@gmail.com>
2018-07-02 10:41:32 -0500

    Model

---
+++ SuperHeroCreator/Models/Hero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperHeroCreator.Models
+{
+    public class Hero
+    {
+        [Key]
+        public int ID { get; set; }
+        public string HeroName { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimaryAbility { get; set; }
+        public string SecondaryAbility { get; set; }
+        public string Catchphrase { get; set; }
+    }
+}
